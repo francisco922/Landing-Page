2b677980b32e0807c79bb85a4baeb33947cdfc2a
francisco1423mkt <franciscomaitre@outlook.com>
2023-08-11 09:15:03 -0300

    aula 11 em manutenção

---
+++ aula11/ex11.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é $(vel)km/h`)
+if(var < 60) { console:console
+    console.log('você ulrtrapassou o limite de velocidade!MULTADO1')
+}
+console.log('dirija sempre usando cinto')
